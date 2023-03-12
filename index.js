c284d74e180e2a27d79e652d7ff3e9c5b195c7b5
Raftouch <rafanelkin@gmail.com>
2023-03-12 18:33:30 +0100

    :sparkles: created routes & views

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require('express')
+const articleRouter = require('./routes/articleRoute')
 const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
+app.get('/', (req,res) => {
+    res.render('index', {text: 'Hello'})
+})
 
 app.listen(5000, () => console.log('Connected to server'))
--- readme.md
+++ readme.md
@@ -3,4 +3,9 @@
 - [x] install dev dependencies
 - [x] create our index.js
 - [x] project setup ('connected to server')
-- [ ]
+- [x] check connection (app.get)
+- [x] set view (app.set)
+- [x] create our index.ejs view
+- [x] create routes
+- [x] output text to the screen (test)
+- [ ] 
