309127ec356bdf6e24ea1b86e2bdaca4388af73c
Raftouch <rafanelkin@gmail.com>
2023-06-02 14:04:38 +0200

    :art: improved structure

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+  require("dotenv").config();
 }
 
-const express = require('express')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articleRoute')
-const methodOverride = require('method-override')
-const app = express()
-const port = process.env.PORT
+const express = require("express");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const articleRouter = require("./routes/articleRoute");
+const methodOverride = require("method-override");
+const app = express();
+const port = process.env.PORT;
 
-mongoose.connect(process.env.MONGO_URL, { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 // useNewUrlParser & useUnifiedTopology --> not obligatory, only if Warning
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-app.get('/', async (req,res) => {
-    const articles = await Article.find().sort({createdAt: 'desc'})
-    res.render('articles/index', { articles: articles })
-})
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
 
-app.use('/articles', articleRouter)
+app.use("/articles", articleRouter);
 
-app.listen(port, () => console.log(`App is listening on port ${port}`))
+app.listen(port, () => console.log(`App is listening on port ${port}`));
